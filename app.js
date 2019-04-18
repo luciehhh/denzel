711d1b4ea1d967c42ab45f982d9cea9c7960b265
luciehhh <lucie.heurte@outlook.fr>
2019-04-18 13:32:17 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const Express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+
+const CONNECTION_URL = "mongodb+srv://luciehhh:<mongodb-4cin-28795692>@webappdenzel-unq2z.mongodb.net/test?retryWrites=true";
+const DATABASE_NAME = "example";
+
+var app = Express();
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+var database, collection;
+
+app.listen(9292, () => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("people");
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
